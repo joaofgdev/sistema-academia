780e9b2ff8e2a82b0138cf7a7527cbd56f7a694b
joaofgdev <jotafagundezz@gmail.com>
2025-08-27 17:07:25 -0300

    add amplify

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,35 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { apiComCriptografia } from './functions/resourse'; 
+// 1. CORREÇÃO: Importe o 'Effect' junto com o 'PolicyStatement'
+import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam'; 
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  apiComCriptografia, 
 });
+
+const alunoTableArn = backend.data.resources.tables.Aluno.tableArn;
+
+// Adicionando a política de permissão para o DynamoDB
+backend.apiComCriptografia.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    // 2. CORREÇÃO: Use o tipo 'Effect.ALLOW' em vez do texto "Allow"
+    effect: Effect.ALLOW,
+    actions: ['dynamodb:PutItem', 'dynamodb:GetItem'],
+    resources: [alunoTableArn],
+  })
+);
+
+// Adicionando a política de permissão para o KMS
+// **SUBSTITUA 'us-east-1' E 'SEU_ACCOUNT_ID' PELOS SEUS DADOS!**
+backend.apiComCriptografia.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    // 3. CORREÇÃO: Use o tipo 'Effect.ALLOW' aqui também
+    effect: Effect.ALLOW,
+    actions: ['kms:Encrypt', 'kms:Decrypt', 'kms:GenerateDataKey'],
+    resources: ['arn:aws:kms:us-east-1:0726-7421-7150:alias/chave-academia-dynamo'],
+  })
+);
